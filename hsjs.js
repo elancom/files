7069c02a76f05c534d47cf6a191d59266cb063f6
elancom <272926206@qq.com>
2020-10-10 15:22:22 +0800

    a
    a

--- hsjs.js
+++ hsjs.js
@@ -1,16 +1,11 @@
 
-// threads.start(function () {
-//     setInterval(() => { check_hb() }, 5 * 1000);
-// });
 
 while (true) {
     main();
-    // check_hb();
-    // sleep(1000);
+
 }
 
 function main() {
-    // 随机等待
     var sleepSec = random(8, 15)
     var r = random(1, 100) // *%
     if (r <= 0) { // 2%
@@ -34,21 +29,17 @@ function main() {
     toast("开始滑动完成");
     hua_dong();
 
-    // 检查广告
     sleep(500);
     if (check_hb()) {
         hua_dong()
     }
 }
 
-// 滑动
 function hua_dong() {
     swipe(device.width / 2 + random(-100, 100), device.height * 0.8, device.width / 2 + random(-100, 100), device.height * 0.02, random(800, 1000));
 }
 
-// 红包检查
 function check_hb() {
-    // 查找广告字眼
     if (className("TextView").textContains("广告").exists()) {
         toastLog("跳过广告")
         return true;
